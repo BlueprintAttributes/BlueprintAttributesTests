a77b7e2469820ae74ca91665e492b2ad0235eb81
Mickael Daniel <daniel.mickael@gmail.com>
2023-01-16 12:47:09 +0100

    Tests: Scaffold `BlueprintAttributes.Editor.GBAEditorSubsystem` spec

---
+++ Source/BlueprintAttributesTests/Private/Tests/Editor/GBAEditorSubsystem.spec.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 2021-2022 Mickael Daniel. All Rights Reserved.
+
+#include "Misc/AutomationTest.h"
+
+BEGIN_DEFINE_SPEC(FGBAEditorSubsystemSpec, "BlueprintAttributes.Editor.GBAEditorSubsystem", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+
+END_DEFINE_SPEC(FGBAEditorSubsystemSpec)
+
+void FGBAEditorSubsystemSpec::Define()
+{
+	Describe(TEXT("Handle K2Nodes on Attribute rename"), [this]()
+	{
+		It(TEXT("should update all K2 Nodes with a FGameplayAttribute Pin parameter"), [this]()
+		{
+			AddInfo(FString::Printf(TEXT("Test stuff here")));
+		});
+	});
+}
